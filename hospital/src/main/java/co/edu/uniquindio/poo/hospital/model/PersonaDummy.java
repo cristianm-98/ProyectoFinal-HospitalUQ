0fe42ca749aba3928a4782db05b2a69582b4772a
Mafe1324 <mariaf.huertasm@uqvirtual.edu.co>
2025-05-24 22:09:26 -0500

    Se realizo la instancia de la prueba unitaria de la clase Persona, como PersonaTest, para probar la creacion de la clase, getters y setters basico, asi como tambien sus atributos.

---
+++ hospital/src/main/java/co/edu/uniquindio/poo/hospital/model/PersonaDummy.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.poo.hospital.model;
+
+public class PersonaDummy extends Persona {
+    public PersonaDummy(String id, String nombre, int edad, String telefono, String direccion, Usuario theUsuario) {
+        super(id, nombre, edad, telefono, direccion, theUsuario);
+    }
+}
+
