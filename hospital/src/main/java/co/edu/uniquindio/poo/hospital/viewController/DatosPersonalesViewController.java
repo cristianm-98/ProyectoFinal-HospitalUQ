c21de3aa1a4fbaf576acc0134762948a3fc3d737
Cristian M <ccmoralesr@uqvirtual.edu.co>
2025-05-25 15:45:37 -0500

    Modificacion del diseño de Datos personales de la vista

--- hospital/src/main/java/co/edu/uniquindio/poo/hospital/viewController/DatosPersonalesViewController.java
+++ hospital/src/main/java/co/edu/uniquindio/poo/hospital/viewController/DatosPersonalesViewController.java
@@ -2,18 +2,12 @@ package co.edu.uniquindio.poo.hospital.viewController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import co.edu.uniquindio.poo.hospital.App;
-import co.edu.uniquindio.poo.hospital.model.Paciente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class DatosPersonalesViewController {
-    private App app;
 
     @FXML
     private ResourceBundle resources;
@@ -26,36 +20,6 @@ public class DatosPersonalesViewController {
 
     @FXML
     private Button btnAtras;
-
-    @FXML
-    private Button btnEliminar;
-
-    @FXML
-    private Button btnLimpiar;
-
-    @FXML
-    private Button btnRegistrar;
-
-    @FXML
-    private TableColumn<?, ?> columDireccion;
-
-    @FXML
-    private TableColumn<?, ?> columEdad;
-
-    @FXML
-    private TableColumn<?, ?> columIdentificacion;
-
-    @FXML
-    private TableColumn<?, ?> columNombre;
-
-    @FXML
-    private TableColumn<?, ?> columTelefono;
-
-    @FXML
-    private TableColumn<?, ?> columTipoSangre;
-
-    @FXML
-    private TableView<?> tblPaciente;
 
     @FXML
     private TextField txtDireccion;
@@ -86,34 +50,9 @@ public class DatosPersonalesViewController {
     }
 
     @FXML
-    void onEliminar(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onLimpiar(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onRegistrar(ActionEvent event) {
-
-    }
-
-    @FXML
     void initialize() {
         assert btnActualizar != null : "fx:id=\"btnActualizar\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
         assert btnAtras != null : "fx:id=\"btnAtras\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
-        assert btnEliminar != null : "fx:id=\"btnEliminar\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
-        assert btnLimpiar != null : "fx:id=\"btnLimpiar\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
-        assert btnRegistrar != null : "fx:id=\"btnRegistrar\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
-        assert columDireccion != null : "fx:id=\"columDireccion\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
-        assert columEdad != null : "fx:id=\"columEdad\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
-        assert columIdentificacion != null : "fx:id=\"columIdentificacion\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
-        assert columNombre != null : "fx:id=\"columNombre\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
-        assert columTelefono != null : "fx:id=\"columTelefono\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
-        assert columTipoSangre != null : "fx:id=\"columTipoSangre\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
-        assert tblPaciente != null : "fx:id=\"tblPaciente\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
         assert txtDireccion != null : "fx:id=\"txtDireccion\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
         assert txtEdad != null : "fx:id=\"txtEdad\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
         assert txtIdentificacion != null : "fx:id=\"txtIdentificacion\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
@@ -122,12 +61,5 @@ public class DatosPersonalesViewController {
         assert txtTipoSangre != null : "fx:id=\"txtTipoSangre\" was not injected: check your FXML file 'crudDatosPersonales.fxml'.";
 
     }
-    public void setApp(App app) {
-        this.app = app;
 
-    }
-
-    public void initPaciente(Paciente paciente) {
-
-    }
 }
